9a00d3b353fe2e6f03def5531fd3b830638c9ca9
Anna Sours <annasours@Annas-MacBook-Pro.local>
2020-12-22 13:51:34 -0700

    pybank read file and store date and profit_losses as lists

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,35 @@
 # import csv and read
+import os
+import csv
+
+pybank_path = os.path.join('Resources','budget_data.csv')
+
+csvfile = open(pybank_path)
+
+csvreader = csv.reader(csvfile, delimiter=',')
+
+# print(csvreader)
+# read header row first
+csv_header = next(csvreader)
+print(f'CSV Header: {csv_header}')
+
 # take data from two columns: date and profit/losses
-# store in containers
+# store date and profit/losses in containers
+date = []
+profit_losses = []
+
+# iterate through each row
+# and append to new lists
+
+
+for row in csvreader:
+    date.append(row[0])
+    profit_losses.append(row[1])
+
+# print(f'date is {date[0]} and profit/losses is {profit_losses[0]}')
+# print(profit_losses[0])
+print(f'{len(date)}{len(profit_losses)}')
+
 # write to new file? where does this need to happen? before or after calculations? I think it happens after...- directions just say export text file with results
 # calcluate:
     # total number of months in dataset (len)
