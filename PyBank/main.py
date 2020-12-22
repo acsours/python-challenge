c46356c9d18d74d82716a743d8c0962a5e6dfd7e
Anna Sours <annasours@Annas-MacBook-Pro.local>
2020-12-22 12:14:55 -0700

    initial pseudocode

---
+++ PyBank/main.py
@@ -0,0 +1,20 @@
+# import csv and read
+# take data from two columns: date and profit/losses
+# store in containers
+# write to new file? where does this need to happen? before or after calculations? I think it happens after...- directions just say export text file with results
+# calcluate:
+    # total number of months in dataset (len)
+    # net total amount of profit/losses over entire period
+    # calculate changes in profit/losses over entire period, then find average of those changes
+    # greatest increase in profits (date and amount) over entire period
+    # greatest decrease in losses (date and amount) over entire period
+'''
+Financial Analysis
+----------------------------
+Total Months: 86
+Total: $38382578
+Average  Change: $-2315.12
+Greatest Increase in Profits: Feb-2012 ($1926159)
+Greatest Decrease in Profits: Sep-2013 ($-2196167)
+'''
+# print analysis to terminal and export text file with results
