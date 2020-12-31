dded266442610eff54b1833aeb10789af8d98b2a
Anna Sours <annasours@Annas-MacBook-Pro.local>
2020-12-31 10:59:15 -0700

    commented with debugging removed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,7 +5,7 @@ import csv
 csvpath = os.path.join('Resources', 'election_data.csv') 
 
 
-# declare some variables
+# declare variables
 vote_count = 0
 vote_percentage = 0
 khan_vote = 0
@@ -15,23 +15,19 @@ otooley_vote = 0
 popular_winner = ""
 candidate = ""
 
-# create a dictionary to hold candidate names
-candidate_list = {}
-
+# Read csv file, skip header
 with open(csvpath) as csvfile: 
     csvreader = csv.reader(csvfile, delimiter=',')
-
     csv_header = next(csvreader)
-    # print(f'CSV Header: {csv_header}')
-
 
 
     for each_row in csvreader:
-        # calculate total number votes cast
+        
+        # calculate total number votes cast and pull candidate value 
         vote_count += 1
         candidate = each_row[2]
+    
         # calculate complete list of candidates who received votes
-        # check value at each_row[2]
         if candidate == "Khan":
             khan_vote += 1
         if candidate == "Correy":
@@ -41,26 +37,27 @@ with open(csvpath) as csvfile:
         if candidate == "O'Tooley":
             otooley_vote += 1
     
-
+    # use candidate vote totals to calculate percentages
     khan_percentage = (khan_vote/vote_count)*100
     correy_percentage = (correy_vote/vote_count)*100
     li_percentage = (li_vote/vote_count)*100
     otooley_percentage = (otooley_vote/vote_count)*100
     
+    # Calculate popular vote winner
     if khan_vote > correy_vote and khan_vote > li_vote and khan_vote > otooley_vote:
         popular_winner = "Khan"
 
     if correy_vote > khan_vote and correy_vote > li_vote and correy_vote > otooley_vote:
         popular_winner = "Correy"
-  
+
     if li_vote > khan_vote and li_vote > correy_vote and li_vote > otooley_vote:
-        popular_winner = "Li"
+        popular_winner = "Li"    
     
     if otooley_vote > khan_vote and otooley_vote > li_vote and otooley_vote > correy_vote:
         popular_winner = "O'Tooley"
     
-    
-    
+
+    # print results in terminal
     output = f"""
     Election Results
     -------------------------------
@@ -78,7 +75,6 @@ with open(csvpath) as csvfile:
 
 
 # export text file with results
-
 output_path = os.path.join('analysis', 'PyPoll.txt')
 
 with open(output_path, 'w') as txtfile:
