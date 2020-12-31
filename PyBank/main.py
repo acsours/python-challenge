0c66bddf9edbe4f95509ef97c8eff25ba2d4bf32
Anna Sours <annasours@Annas-MacBook-Pro.local>
2020-12-31 11:11:45 -0700

    PyBank main.py commented and debugging deleted

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,4 @@
-# import csv and read
+# import and read csv
 import os
 import csv
 
@@ -10,23 +10,11 @@ csvreader = csv.reader(csvfile, delimiter=',')
 
 # read header row first
 csv_header = next(csvreader)
-# print(f'CSV Header: {csv_header}')
 
-# take data from two columns: date and profit/losses
-# store date and profit/losses in containers
+# create new lists for data from csv
 date = []
 profit_losses = []
 
-# iterate through each row
-# and append to new lists
-
-
-for row in csvreader:
-    date.append(row[0])
-    profit_losses.append(row[1])
-
-# zip together into a single tuple
-pybank_list = list(zip(date, profit_losses))
 
 # declare variables
 net_total = 0
@@ -41,33 +29,29 @@ profit_decrease_date = ""
 
 # total number of months in dataset (len)
 total_months = len(date)
-#print('Total months: ' + str(total_months))
 
 
 # calculate net total of profits/losses over entire period
-for each_month in pybank_list:
-    #print(each_month[1])
+for each_month in csvreader:
     current_finances = int(each_month[1])
     net_total += current_finances
-    # print(net_total)
-    # exit()
+    
 
-# calculate changes in profit/losses over entire period, then find average of those changes
+# calculate changes in profit/losses over entire period
     if previous_finances != 0:
         
         value_change = current_finances - previous_finances
         cum_value_change += value_change
         row_index += 1
-        # print(f'Value change is {value_change}')
 
     previous_finances = current_finances
+    
     # find average of changes
     average_change = round((cum_value_change / (total_months - 1)), 2)
     
     # greatest increase in profits (date and amount) over entire period
     if value_change > profit_increase:
         profit_increase = value_change
-        #need to pull value from each_month[0]
         profit_increase_date = each_month[0]
 
     # greatest decrease in losses (date and amount) over entire period
@@ -75,6 +59,7 @@ for each_month in pybank_list:
         profit_decrease = value_change
         profit_decrease_date = each_month[0]
 
+# print analyis results to terminal
 output = f"""
 Financial Analysis
 ------------------------------
